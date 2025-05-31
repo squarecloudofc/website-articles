6eea035fb604447c9a3bb08dd3c3397d0a13fc5a
Richard Martins <me@richaar.dev>
2025-05-30 23:31:10 -0300

    chore: generateMissingPostIDs should ensure that every metadata follows the structure

--- scripts/generateMissingPostIDs.js
+++ scripts/generateMissingPostIDs.js
@@ -9,14 +9,14 @@ async function main() {
     const articles = await getArticles(project)
 
     articles.forEach(article => {
-      if (article.id && article.id?.length === 12) return
-
+      const articleId = article.id === "" || article.id?.length !== 12 ? generatePostID() : article.id
       const content = {
         "$schema": "https://gist.githubusercontent.com/richaardev/a5fec5b851f33166757e3827b03ddded/raw/e942bbdf03b0128c0b6b189e299843cc57734588/schema.json",
-        id: article.id ?? generatePostID(),
+        id: articleId,
         updated_at: article?.updated_at,
         metadata: article?.metadata,
       }
+      // console.log(article.path, content)
       // console.log(content, article)
 
       writeFileSync(join(project, article.path, "metadata.json"), JSON.stringify(content, undefined, "  "))
